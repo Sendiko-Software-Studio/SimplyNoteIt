3a372e29796597c3ccfe74ab37593b9fb43ac84d
Sendiko <sarangtawon897@gmail.com>
2024-01-22 21:12:38 +0700

    enc(crea): events for SignUpScreen

---
+++ app/src/main/java/com/sendiko/simplynoteit/presentation/ui/screen/signup/SignUpScreenEvent.kt
@@ -0,0 +1,12 @@
+package com.sendiko.simplynoteit.presentation.ui.screen.signup
+
+sealed class SignUpScreenEvent {
+    data class OnUsernameChanged(val username: String): SignUpScreenEvent()
+    data class OnEmailChanged(val email: String): SignUpScreenEvent()
+    data class OnPasswordChanged(val password: String): SignUpScreenEvent()
+    data object OnUsernameCleared: SignUpScreenEvent()
+    data object OnPasswordCleared: SignUpScreenEvent()
+    data object OnEmailCleared: SignUpScreenEvent()
+    data class OnPasswordVisibilityChanged(val isVisible: Boolean): SignUpScreenEvent()
+    data class OnSignUp(val username: String, val password: String): SignUpScreenEvent()
+}
