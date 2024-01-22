f2d613610b50e47e37c404fb5ea1fc1a931a7a4b
Sendiko <sarangtawon897@gmail.com>
2024-01-22 21:15:17 +0700

    enc(crea): add routing and navigation

--- app/src/main/java/com/sendiko/simplynoteit/MainActivity.kt
+++ app/src/main/java/com/sendiko/simplynoteit/MainActivity.kt
@@ -3,21 +3,70 @@ package com.sendiko.simplynoteit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.sendiko.simplynoteit.presentation.ui.screen.WelcomeScreen
+import com.sendiko.simplynoteit.presentation.ui.screen.navigation.Destinations
+import com.sendiko.simplynoteit.presentation.ui.screen.signin.SignInScreen
+import com.sendiko.simplynoteit.presentation.ui.screen.signin.SignInScreenViewModel
+import com.sendiko.simplynoteit.presentation.ui.screen.signup.SignUpScreen
+import com.sendiko.simplynoteit.presentation.ui.screen.signup.SignUpScreenViewModel
 import com.sendiko.simplynoteit.presentation.ui.theme.SimplyNoteItTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             SimplyNoteItTheme {
-                
+                NavHost(
+                    navController = navController,
+                    startDestination = Destinations.WelcomeScreenDestination.destination,
+                    builder = {
+                        composable(
+                            route = Destinations.WelcomeScreenDestination.destination,
+                            content = {
+                                WelcomeScreen { destination ->
+                                    navController.navigate(destination)
+                                }
+                            }
+                        )
+                        composable(
+                            route = Destinations.SignInScreenDestination.destination,
+                            content = {
+                                val viewModel: SignInScreenViewModel = hiltViewModel()
+                                SignInScreen(
+                                    state = viewModel.state.collectAsState().value,
+                                    onEvents = viewModel::onEvent,
+                                    onNavigate = {
+                                        navController.navigate(it)
+                                    }
+                                )
+                            }
+                        )
+                        composable(
+                            route = Destinations.SignUpScreenDestination.destination,
+                            content = {
+                                val viewModel: SignUpScreenViewModel = viewModel()
+                                SignUpScreen(
+                                    state = viewModel.state.collectAsState().value,
+                                    onEvents = viewModel::onEvent,
+                                    onNavigate = {
+                                        navController.navigate(it)
+                                    }
+                                )
+                            }
+                        )
+                    }
+                )
             }
         }
     }
