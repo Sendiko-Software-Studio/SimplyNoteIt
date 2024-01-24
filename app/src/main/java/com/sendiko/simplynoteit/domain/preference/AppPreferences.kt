1f4a37390b7ce158f64e989934a23fca9f5ed127
Sendiko <sarangtawon897@gmail.com>
2024-01-24 13:12:10 +0700

    enc(crea): AppPreferences

---
+++ app/src/main/java/com/sendiko/simplynoteit/domain/preference/AppPreferences.kt
@@ -0,0 +1,39 @@
+package com.sendiko.simplynoteit.domain.preference
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class AppPreferences @Inject constructor(private val dataStore: DataStore<Preferences>) {
+
+    private val nameKey = stringPreferencesKey("username")
+    private val tokenKey = stringPreferencesKey("auth_token")
+
+    fun getName(): Flow<String> {
+        return dataStore.data.map {
+            it[nameKey]?:""
+        }
+    }
+
+    suspend fun setName(name: String) {
+        dataStore.edit {
+            it[nameKey] = name
+        }
+    }
+
+    fun getToken(): Flow<String> {
+        return dataStore.data.map {
+            it[tokenKey]?:""
+        }
+    }
+
+    suspend fun setToken(token: String){
+        dataStore.edit {
+            it[tokenKey] = token
+        }
+    }
+}
