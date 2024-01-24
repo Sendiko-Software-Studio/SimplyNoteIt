b03d2954dc73c85c21fff9f52a375a0f236e63d2
Sendiko <sarangtawon897@gmail.com>
2024-01-24 13:17:16 +0700

    enc(add): get and save username and token

--- app/src/main/java/com/sendiko/simplynoteit/domain/repositories/UserRepositories.kt
+++ app/src/main/java/com/sendiko/simplynoteit/domain/repositories/UserRepositories.kt
@@ -3,11 +3,20 @@ package com.sendiko.simplynoteit.domain.repositories
 import com.sendiko.simplynoteit.data.ApiService
 import com.sendiko.simplynoteit.data.requests.SignInRequest
 import com.sendiko.simplynoteit.data.requests.SignUpRequest
+import com.sendiko.simplynoteit.domain.preference.AppPreferences
 import javax.inject.Inject
 
-class UserRepository @Inject constructor(private val client: ApiService) {
+class UserRepository @Inject constructor(private val client: ApiService, private val preferences: AppPreferences) {
 
     fun signUp(request: SignUpRequest) = client.signUp(request)
 
     fun signIn(request: SignInRequest) = client.signIn(request)
+
+    suspend fun saveUsername(username: String) = preferences.setName(username)
+
+    fun getUsername() = preferences.getName()
+
+    suspend fun setToken(token: String) = preferences.setToken(token)
+
+    fun getToken() = preferences.getToken()
 }
