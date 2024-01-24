236f817fe692e8f4c267b24bbdc2d905888f8781
Sendiko <sarangtawon897@gmail.com>
2024-01-24 13:12:29 +0700

    enc(add): adjust notification composable

---
+++ app/src/main/java/com/sendiko/simplynoteit/presentation/ui/components/ContentBoxWithNotification.kt
@@ -0,0 +1,99 @@
+package com.sendiko.simplynoteit.presentation.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sendiko.simplynoteit.presentation.ui.theme.nunitoFont
+
+@Composable
+fun ContentBoxWithNotification(
+    message: String,
+    isVisible: Boolean,
+    isErrorNotification: Boolean = false,
+    isLoading: Boolean = false,
+    content: @Composable (() -> Unit),
+) {
+    Box {
+        content()
+        Notification(
+            message = message,
+            isVisible = isVisible && !isLoading,
+            isErrorNotification = isErrorNotification
+        )
+        LoadingIndicator(isLoading = isLoading)
+    }
+}
+
+@Composable
+fun Notification(
+    message: String,
+    isVisible: Boolean,
+    isErrorNotification: Boolean = false,
+) {
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = expandVertically(),
+        exit = shrinkVertically()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(if (isErrorNotification) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.surfaceVariant),
+            content = {
+                Text(
+                    text = message,
+                    modifier = Modifier.padding(top = 28.dp + 8.dp, end = 8.dp, start = 8.dp, bottom = 16.dp),
+                    color = if (isErrorNotification) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.onSurfaceVariant,
+                    fontFamily = nunitoFont
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun LoadingIndicator(
+    isLoading: Boolean,
+) {
+
+    AnimatedVisibility(
+        visible = isLoading,
+        enter = expandVertically(),
+        exit = shrinkVertically()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            content = {
+                Row(
+                    modifier = Modifier.padding(top = 28.dp + 8.dp, end = 8.dp, start = 8.dp, bottom = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Loading",
+                        color = MaterialTheme.colorScheme.onTertiaryContainer,
+                        fontFamily = nunitoFont
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    CircularProgressIndicator(modifier = Modifier.size(24.dp), strokeWidth = 1.dp)
+                }
+            }
+        )
+    }
+}
