d3a3c4efa10875170a7b73c8854aa20d7d8d6444
Sendiko <sarangtawon897@gmail.com>
2024-01-24 13:14:11 +0700

    enc(crea): PreferenceDatastore declaration

---
+++ app/src/main/java/com/sendiko/simplynoteit/domain/preference/PreferenceDatastore.kt
@@ -0,0 +1,8 @@
+package com.sendiko.simplynoteit.domain.preference
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore("app_preferences")
