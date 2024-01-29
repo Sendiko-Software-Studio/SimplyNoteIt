5f6f1d3c8464bf0cf7ccdd45f749f1114e75700b
Sendiko <sarangtawon897@gmail.com>
2024-01-29 18:26:01 +0700

    enc(add): update response

--- app/src/main/java/com/sendiko/simplynoteit/data/responses/GetTasksResponse.kt
+++ app/src/main/java/com/sendiko/simplynoteit/data/responses/GetTasksResponse.kt
@@ -1,18 +1,39 @@
 package com.sendiko.simplynoteit.data.responses
 
+import com.google.gson.annotations.SerializedName
+
 data class GetTasksResponse(
+
+	@field:SerializedName("task")
 	val task: List<TaskItem>,
+
+	@field:SerializedName("message")
 	val message: String,
+
+	@field:SerializedName("status")
 	val status: Int
 )
 
 data class TaskItem(
-	val isDone: Int,
+
+	@field:SerializedName("is_done")
+	val isDone: String,
+
+	@field:SerializedName("updated_at")
 	val updatedAt: String,
-	val userId: Int,
+
+	@field:SerializedName("user_id")
+	val userId: String,
+
+	@field:SerializedName("description")
 	val description: String,
+
+	@field:SerializedName("created_at")
 	val createdAt: String,
+
+	@field:SerializedName("id")
 	val id: Int,
+
+	@field:SerializedName("title")
 	val title: String
 )
-
