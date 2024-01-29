b3b6ca3d65371f84e9d1f4d98319715be3868d6d
Sendiko <sarangtawon897@gmail.com>
2024-01-29 18:25:13 +0700

    enc(add): add more events

--- app/src/main/java/com/sendiko/simplynoteit/presentation/ui/screen/dashboard/DashboardScreenEvents.kt
+++ app/src/main/java/com/sendiko/simplynoteit/presentation/ui/screen/dashboard/DashboardScreenEvents.kt
@@ -3,12 +3,15 @@ package com.sendiko.simplynoteit.presentation.ui.screen.dashboard
 import com.sendiko.simplynoteit.data.responses.TaskItem
 
 sealed class DashboardScreenEvents {
-    data class OnCheckChange(val isChecked: Boolean): DashboardScreenEvents()
+    data class OnCheckChange(val task: TaskItem): DashboardScreenEvents()
     data class OnTaskClick(val task: TaskItem): DashboardScreenEvents()
     data class OnTaskTitleChange(val title: String): DashboardScreenEvents()
+    data object OnTaskTitleClear: DashboardScreenEvents()
     data class OnTaskDescriptionChange(val description: String): DashboardScreenEvents()
-    data class OnCreateTask(val task: TaskItem): DashboardScreenEvents()
-    data class OnUpdateTask(val task: TaskItem): DashboardScreenEvents()
+    data object OnTaskDescClear: DashboardScreenEvents()
+    data object OnCreateTask : DashboardScreenEvents()
+    data object OnUpdateTask : DashboardScreenEvents()
     data class OnSetCheckedTaskVisible(val isVisible: Boolean): DashboardScreenEvents()
-    data class OnTaskSheetVisibilityChanged(val isVisible: Boolean): DashboardScreenEvents()
+    data class OnTaskSheetVisibilityChanged(val isVisible: Boolean, val taskAction: TaskAction): DashboardScreenEvents()
+    data object OnTaskLoad: DashboardScreenEvents()
 }
