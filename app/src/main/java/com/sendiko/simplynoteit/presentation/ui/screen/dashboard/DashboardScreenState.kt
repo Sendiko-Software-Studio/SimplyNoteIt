73deed3467d85bf11b147c4d2e5ee992df401ebf
Sendiko <sarangtawon897@gmail.com>
2024-01-29 18:25:29 +0700

    enc(add): extract states for task sheet

--- app/src/main/java/com/sendiko/simplynoteit/presentation/ui/screen/dashboard/DashboardScreenState.kt
+++ app/src/main/java/com/sendiko/simplynoteit/presentation/ui/screen/dashboard/DashboardScreenState.kt
@@ -1,19 +1,18 @@
 package com.sendiko.simplynoteit.presentation.ui.screen.dashboard
 
 import com.sendiko.simplynoteit.data.responses.TaskItem
-import com.sendiko.simplynoteit.presentation.ui.helper.ErrorTextField
+import com.sendiko.simplynoteit.presentation.ui.helper.FailedRequest
 
 data class DashboardScreenState(
     val tasks: List<TaskItem> = emptyList(),
     val name: String = "",
-    val isTaskSheetExpanded: Boolean = false,
-    val taskText: String = "",
-    val isTaskFieldError: ErrorTextField = ErrorTextField(),
-    val descriptionText: String = "",
-    val isDescriptionFieldError: ErrorTextField = ErrorTextField(),
+    val taskSheetState: TaskSheetState = TaskSheetState(),
     val sortBy: SortBy = SortBy.ID,
     val isCheckedTaskVisible: Boolean = false,
     val task: TaskItem? = null,
     val token: String = "",
     val notificationMessage: String = "",
+    val isRequestFailed: FailedRequest = FailedRequest(),
+    val isLoading: Boolean = false,
+    val userId: String = "",
 )
