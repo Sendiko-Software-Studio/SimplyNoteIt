7de9bb0f960feb397f5b15b938d86e64a004211d
Sendiko <sarangtawon897@gmail.com>
2024-02-02 17:21:11 +0700

    enc(add): implement card and topappbar scrollable

--- app/src/main/java/com/sendiko/simplynoteit/presentation/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/sendiko/simplynoteit/presentation/ui/screen/profile/ProfileScreen.kt
@@ -3,12 +3,14 @@ package com.sendiko.simplynoteit.presentation.ui.screen.profile
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ContactPage
 import androidx.compose.material.icons.filled.Info
@@ -21,9 +23,12 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -31,8 +36,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sendiko.simplynoteit.R
+import com.sendiko.simplynoteit.presentation.ui.components.ABOUT_US
+import com.sendiko.simplynoteit.presentation.ui.components.CONTACT_ME
 import com.sendiko.simplynoteit.presentation.ui.components.ContentBoxWithNotification
 import com.sendiko.simplynoteit.presentation.ui.components.FilledButton
+import com.sendiko.simplynoteit.presentation.ui.components.InfoCard
+import com.sendiko.simplynoteit.presentation.ui.components.InfoCardWithIcon
+import com.sendiko.simplynoteit.presentation.ui.components.PRIVACY_POLICY
 import com.sendiko.simplynoteit.presentation.ui.components.RedButton
 import com.sendiko.simplynoteit.presentation.ui.screen.navigation.Destinations
 import com.sendiko.simplynoteit.presentation.ui.theme.nunitoFont
@@ -44,6 +54,8 @@ fun ProfileScreen(
     onEvent: (ProfileScreenEvent) -> Unit,
     onNavigate: (String) -> Unit,
 ) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
     LaunchedEffect(
         key1 = state.isSignedOutSuccessfully,
         key2 = state.name,
@@ -55,174 +67,115 @@ fun ProfileScreen(
                 onEvent(ProfileScreenEvent.OnGetUserInfo)
         }
     )
-    Scaffold(
-        topBar = {
-            LargeTopAppBar(
-                title = {
-                    Text(
-                        text = "Your Profile",
-                        fontFamily = nunitoFont,
-                        fontWeight = FontWeight.SemiBold
-                    )
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = { onNavigate(Destinations.DashboardScreenDestination.destination) },
-                        content = {
-                            Icon(
-                                imageVector = Icons.Default.KeyboardArrowLeft,
-                                contentDescription = "navigate back"
-                            )
-                        }
-                    )
-                }
-            )
-        }
+    ContentBoxWithNotification(
+        message = state.notificationMessage,
+        isLoading = state.isLoading,
+        isErrorNotification = state.isRequestFailed.isFailed
     ) {
-        ContentBoxWithNotification(
-            message = state.notificationMessage,
-            isLoading = state.isLoading,
-            isErrorNotification = state.isRequestFailed.isFailed
+        Scaffold(
+            topBar = {
+                LargeTopAppBar(
+                    title = {
+                        Text(
+                            text = "Your Profile",
+                            fontFamily = nunitoFont,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = { onNavigate(Destinations.DashboardScreenDestination.destination) },
+                            content = {
+                                Icon(
+                                    imageVector = Icons.Default.KeyboardArrowLeft,
+                                    contentDescription = "navigate back"
+                                )
+                            }
+                        )
+                    },
+                    scrollBehavior = scrollBehavior
+                )
+            }
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(
+            ContentBoxWithNotification(
+                message = state.notificationMessage,
+                isLoading = state.isLoading,
+                isErrorNotification = state.isRequestFailed.isFailed
+            ) {
+                LazyColumn(
+                    contentPadding = PaddingValues(
                         start = 16.dp,
                         end = 16.dp,
                         top = it.calculateTopPadding(),
                         bottom = it.calculateBottomPadding()
                     ),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Card {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = "Username: ",
-                            fontFamily = nunitoFont,
-                            fontSize = 16.sp
-                        )
-                        Text(
-                            text = state.name,
-                            fontFamily = nunitoFont,
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Bold
-                        )
+                    modifier = Modifier
+                        .nestedScroll(scrollBehavior.nestedScrollConnection),
+                    content = {
+                        item {
+                            InfoCard(label = "Username: ", text = state.name)
+                            Spacer(modifier = Modifier.height(8.dp))
+                            InfoCard(label = "Email: ", text = state.email)
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                        item {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.spacedBy(16.dp)
+                            ) {
+                                RedButton(
+                                    text = "Logout",
+                                    onClick = {
+                                        onEvent(ProfileScreenEvent.OnLogoutClick)
+                                    },
+                                    modifier = Modifier.weight(1f)
+                                )
+                                FilledButton(
+                                    text = "Change password",
+                                    onClick = { /*TODO*/ },
+                                    modifier = Modifier.weight(1f)
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(16.dp))
+                        }
+                        item {
+                            InfoCardWithIcon(
+                                label = "About us",
+                                icon = Icons.Default.Info,
+                                extraInfo = ABOUT_US
+                            )
+                            InfoCardWithIcon(
+                                label = "Privacy and policy",
+                                icon = Icons.Default.PrivacyTip,
+                                extraInfo = PRIVACY_POLICY
+                            )
+                            InfoCardWithIcon(
+                                label = "Contact us",
+                                icon = Icons.Default.ContactPage,
+                                extraInfo = CONTACT_ME
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                        }
+                        item {
+                            Text(
+                                text = "powered by:",
+                                modifier = Modifier.fillMaxWidth(),
+                                textAlign = TextAlign.Center,
+                                fontFamily = nunitoFont
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Row {
+                                Spacer(modifier = Modifier.weight(1f))
+                                Image(
+                                    painter = painterResource(id = R.drawable.logo_long),
+                                    contentDescription = "logo",
+                                    modifier = Modifier.weight(3f)
+                                )
+                                Spacer(modifier = Modifier.weight(1f))
+                            }
+                        }
                     }
-                }
-                Card {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = "Email: ",
-                            fontFamily = nunitoFont,
-                            fontSize = 16.sp
-                        )
-                        Text(
-                            text = state.email,
-                            fontFamily = nunitoFont,
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                    }
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    RedButton(
-                        text = "Logout",
-                        onClick = {
-                            onEvent(ProfileScreenEvent.OnLogoutClick)
-                        },
-                        modifier = Modifier.weight(1f)
-                    )
-                    FilledButton(
-                        text = "Change password",
-                        onClick = { /*TODO*/ },
-                        modifier = Modifier.weight(1f)
-                    )
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                Card {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = "About us",
-                            fontFamily = nunitoFont,
-                            fontSize = 16.sp
-                        )
-                        Icon(
-                            imageVector = Icons.Default.Info,
-                            contentDescription = "info"
-                        )
-                    }
-                }
-                Card {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = "Privacy and policy",
-                            fontFamily = nunitoFont,
-                            fontSize = 16.sp
-                        )
-                        Icon(
-                            imageVector = Icons.Default.PrivacyTip,
-                            contentDescription = "privacy and policy info"
-                        )
-                    }
-                }
-                Card {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = "Contact us",
-                            fontFamily = nunitoFont,
-                            fontSize = 16.sp
-                        )
-                        Icon(
-                            imageVector = Icons.Default.ContactPage,
-                            contentDescription = "contact"
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(
-                    text = "powered by:",
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    fontFamily = nunitoFont
                 )
-                Row {
-                    Spacer(modifier = Modifier.weight(1f))
-                    Image(
-                        painter = painterResource(id = R.drawable.logo_long),
-                        contentDescription = "logo",
-                        modifier = Modifier.weight(3f)
-                    )
-                    Spacer(modifier = Modifier.weight(1f))
-                }
             }
         }
     }
