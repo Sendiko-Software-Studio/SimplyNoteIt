97f5303ded2983e8755adb4b0425959255a4927c
Sendiko <sarangtawon897@gmail.com>
2024-02-02 07:11:30 +0700

    enc(add): response for signing out

---
+++ app/src/main/java/com/sendiko/simplynoteit/data/responses/SignOutResponse.kt
@@ -0,0 +1,18 @@
+package com.sendiko.simplynoteit.data.responses
+
+import com.google.gson.annotations.SerializedName
+
+data class SignOutResponse(
+
+	@field:SerializedName("message")
+	val message: String? = null,
+
+	@field:SerializedName("token_type")
+	val tokenType: String? = null,
+
+	@field:SerializedName("status")
+	val status: Int? = null,
+
+	@field:SerializedName("token")
+	val token: String? = null
+)
