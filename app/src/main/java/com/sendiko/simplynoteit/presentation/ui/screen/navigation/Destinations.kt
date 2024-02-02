f8e9550e1ef065b2f9e0e50a6bd3570e0fe3fa6f
Sendiko <sarangtawon897@gmail.com>
2024-02-02 07:09:13 +0700

    enc(add): ProfileScreenDestination

--- app/src/main/java/com/sendiko/simplynoteit/presentation/ui/screen/navigation/Destinations.kt
+++ app/src/main/java/com/sendiko/simplynoteit/presentation/ui/screen/navigation/Destinations.kt
@@ -3,10 +3,10 @@ package com.sendiko.simplynoteit.presentation.ui.screen.navigation
 sealed class Destinations(val destination: String) {
 
     data object SplashScreenDestiation: Destinations("splash_screen")
-
     data object WelcomeScreenDestination: Destinations("welcome_screen")
     data object SignInScreenDestination: Destinations("signin_screen")
     data object SignUpScreenDestination: Destinations("signup_screen")
     data object MainGraph: Destinations("main_graph")
     data object DashboardScreenDestination: Destinations("dashboard_screen")
+    data object ProfileScreenDestination: Destinations("profile_screen")
 }
