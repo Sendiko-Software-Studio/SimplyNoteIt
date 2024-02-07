0471039d8918beef6946de4a25ca9fbf82078369
Sendiko <sarangtawon897@gmail.com>
2024-02-07 11:47:25 +0700

    enc(add): set and set sortby

--- app/src/main/java/com/sendiko/simplynoteit/domain/repositories/TaskRepository.kt
+++ app/src/main/java/com/sendiko/simplynoteit/domain/repositories/TaskRepository.kt
@@ -3,9 +3,11 @@ package com.sendiko.simplynoteit.domain.repositories
 import com.sendiko.simplynoteit.data.ApiService
 import com.sendiko.simplynoteit.data.requests.AddTaskRequest
 import com.sendiko.simplynoteit.data.requests.UpdateTaskRequest
+import com.sendiko.simplynoteit.domain.preference.AppPreferences
+import com.sendiko.simplynoteit.presentation.ui.screen.dashboard.SortBy
 import javax.inject.Inject
 
-class TaskRepository @Inject constructor(private val client: ApiService) {
+class TaskRepository @Inject constructor(private val client: ApiService, private val preferences: AppPreferences) {
 
     fun getTasks(token: String) = client.getTasks(token)
 
@@ -14,4 +16,8 @@ class TaskRepository @Inject constructor(private val client: ApiService) {
     fun updateTask(id: String, token: String, request: UpdateTaskRequest) = client.updateTask(id, token, request)
 
     fun deleteTask(id: String, token: String) = client.deleteTask(id, token)
+
+    fun getSortBy() = preferences.getSortBy()
+
+    suspend fun setSortBy(sortBy: SortBy) = preferences.setSortBy(sortBy.name)
 }
